5c2fb1c7ba390274e360136a47057d31d394afd0
dhanush-001 <154492305+dhanush-001@users.noreply.github.com>
2025-01-24 21:57:36 +0530

    Add files via upload

---
+++ remove extra space.py
@@ -0,0 +1,9 @@
+original_paragraph = "     Dhanush will   become a financial freedom      person"
+# strip() methods is used to remove the spaces at start and end of string
+cleaned_paragraph = original_paragraph.strip()
+#split the string into words 
+cleaned_paragraph =' '.join(cleaned_paragraph.split())
+
+print("original_paragraph :",original_paragraph)
+print("cleaned paragraph :",cleaned_paragraph)
+
