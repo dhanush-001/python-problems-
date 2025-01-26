d39883f779bb3ccf377a2123db5374a56947c5b1
dhanush-001 <154492305+dhanush-001@users.noreply.github.com>
2025-01-26 22:32:40 +0530

    Add files via upload

---
+++ sum of the array.py
@@ -0,0 +1,11 @@
+def sum_of_array(arr):
+    total = 0
+
+    for num in arr:
+        total += num
+    return total
+
+array =[2,45,67,8]
+result = sum_of_array(array)
+print("the sum of the array is :",result)
+        
